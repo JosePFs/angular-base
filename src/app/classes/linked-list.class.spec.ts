e57d9109983fb185bb1605943f889135d0a56ff6
jose.puente <jose.puente@netexlearning.com>
2019-09-15 23:26:51 +0200

    Added linked list test

--- src/app/classes/linked-list.class.spec.ts
+++ src/app/classes/linked-list.class.spec.ts
@@ -1,12 +1,75 @@
-import { Node } from 'src/app/classes/node.class';
 import { LinkedList } from './linked-list.class';
 
 describe('LinkedList', () => {
-  it('should create an instance', () => {
-    const linkedList = new LinkedList();
-    expect(linkedList).toBeTruthy();
-    const node = new Node({});
-    linkedList.insertAtBeginning(node);
-    expect(node.data).toEqual({});
+  it('should be able to retrieve and insert nodes', () => {
+    let linkedList: LinkedList;
+
+    linkedList = new LinkedList();
+    let beginningNode = linkedList.insertAtBeginning({ id: 0 });
+    expect(beginningNode.data).toEqual(linkedList.getFirst().data);
+    expect(linkedList.getFirst().data).toEqual(linkedList.getLast().data);
+    beginningNode = linkedList.insertAtBeginning({ id: 1 });
+    expect(beginningNode.data).toEqual(linkedList.getFirst().data);
+    expect(linkedList.getFirst().data).not.toEqual(linkedList.getLast().data);
+
+    linkedList = new LinkedList();
+    let endNode = linkedList.insertAtEnd({ id: 0 });
+    expect(endNode.data).toEqual(linkedList.getFirst().data);
+    expect(linkedList.getFirst().data).toEqual(linkedList.getLast().data);
+    endNode = linkedList.insertAtEnd({ id: 1 });
+    expect(endNode.data).toEqual(linkedList.getLast().data);
+
+    linkedList = new LinkedList();
+    expect(() => linkedList.insertAt(-1, {})).toThrow(Error);
+    expect(() => linkedList.insertAt(2, {})).toThrow(Error);
+    const firstNode = linkedList.insertAt(0, { id: 0 });
+    expect(firstNode.data).toEqual(linkedList.getFirst().data);
+    expect(linkedList.getFirst().data).toEqual(linkedList.getLast().data);
+    const secondNode = linkedList.insertAt(1, { id: 1 });
+    const thirdNode = linkedList.insertAt(2, { id: 2 });
+    expect(thirdNode.data).toEqual(linkedList.getLast().data);
+    const middleNode = linkedList.getAt(1);
+    expect(middleNode.data).toEqual(secondNode.data);
+  });
+
+  it('should be able to delete nodes', () => {
+    let linkedList: LinkedList;
+
+    linkedList = new LinkedList();
+    expect(() => linkedList.deleteFirst()).toThrow(Error);
+    linkedList.insertAt(0, { id: 0 });
+    linkedList.deleteFirst();
+    expect(linkedList.getFirst()).toBeNull();
+    expect(linkedList.getLast()).toBeNull();
+    linkedList.insertAt(0, { id: 0 });
+    const lastnode = linkedList.insertAt(1, { id: 1 });
+    linkedList.deleteFirst();
+    expect(lastnode.data).toEqual(linkedList.getFirst().data);
+
+    linkedList = new LinkedList();
+    expect(() => linkedList.deleteLast()).toThrow(Error);
+    linkedList.insertAt(0, { id: 0 });
+    const middleNode = linkedList.insertAt(1, { id: 1 });
+    linkedList.insertAt(2, { id: 2 });
+    linkedList.deleteLast();
+    expect(middleNode.data).toEqual(linkedList.getLast().data);
+
+    linkedList = new LinkedList();
+    expect(() => linkedList.deleteAt(0)).toThrow(Error);
+    linkedList.insertAt(0, { id: 0 });
+    expect(() => linkedList.deleteAt(-1)).toThrow(Error);
+    expect(() => linkedList.deleteAt(1)).toThrow(Error);
+    expect(() => linkedList.deleteAt(2)).toThrow(Error);
+    linkedList.deleteAt(0);
+    expect(linkedList.getFirst()).toBeNull();
+    expect(linkedList.getLast()).toBeNull();
+    linkedList.insertAt(0, { id: 0 });
+    const nextFirstNode = linkedList.insertAt(1, { id: 1 });
+    const nextLastNode = linkedList.insertAt(2, { id: 2 });
+    linkedList.insertAt(3, { id: 3 });
+    linkedList.deleteAt(0);
+    expect(nextFirstNode.data).toEqual(linkedList.getFirst().data);
+    linkedList.deleteAt(2);
+    expect(nextLastNode.data).toEqual(linkedList.getLast().data);
   });
 });
--- src/app/classes/linked-list.class.ts
+++ src/app/classes/linked-list.class.ts
@@ -2,14 +2,150 @@ import { Node } from './node.class';
 
 export class LinkedList {
   private head: Node = null;
+  private tail: Node = null;
 
   constructor() {}
 
-  insertAtBeginning(data: any) {
+  insertAtBeginning(data: any): Node {
     const newNode = new Node(data);
+    if (null === this.head) {
+      this.head = this.tail = newNode;
+
+      return newNode;
+    }
+
     newNode.next = this.head;
     this.head = newNode;
 
+    return newNode;
+  }
+
+  insertAtEnd(data: any): Node {
+    const newNode = new Node(data);
+    if (null === this.head) {
+      this.head = this.tail = newNode;
+
+      return newNode;
+    }
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+
+    return newNode;
+  }
+
+  insertAt(index: number, data: any): Node {
+    if (index < 0) {
+      throw new Error('Index not valid');
+    }
+
+    if (0 === index) {
+      return this.insertAtBeginning(data);
+    }
+
+    const previous = this.getAt(index - 1);
+    if (null === previous) {
+      throw new Error('Node not found');
+    }
+    const newNode = new Node(data);
+    newNode.next = previous.next;
+    previous.next = newNode;
+    if (previous === this.tail) {
+      this.tail = newNode;
+    }
+
+    return newNode;
+  }
+
+  deleteAt(index: number): Node | null {
+    if (null === this.head) {
+      throw new Error('Empty list');
+    }
+    if (index < 0) {
+      throw new Error('Index not valid');
+    }
+
+    if (0 === index) {
+      this.head = this.head.next;
+      if (null === this.head) {
+        this.tail = null;
+      } else if (null === this.head.next) {
+        this.tail = this.head;
+      }
+
+      return this.head;
+    }
+
+    const previous = this.getAt(index - 1);
+    if (null === previous || null === previous.next) {
+      throw new Error('Node not found');
+    }
+
+    previous.next = previous.next.next;
+    if (null === previous.next) {
+      this.tail = previous;
+    }
+
     return this.head;
   }
+
+  deleteFirst(): Node | null {
+    if (null === this.head) {
+      throw new Error('Empty list');
+    }
+    if (this.head === this.tail) {
+      this.tail = this.head.next;
+    }
+    this.head = this.head.next;
+
+    return this.head;
+  }
+
+  deleteLast(): Node | null {
+    if (null === this.head) {
+      throw new Error('Empty list');
+    }
+
+    if (null === this.head.next) {
+      this.head = this.tail = null;
+
+      return null;
+    }
+    let previous = this.head;
+    let tail = this.head.next;
+
+    while (null !== tail.next) {
+      previous = tail;
+      tail = tail.next;
+    }
+
+    previous.next = null;
+    this.tail = previous;
+
+    return this.tail;
+  }
+
+  getAt(index: number): Node | null {
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+      counter++;
+      node = node.next;
+    }
+
+    return null;
+  }
+
+  getFirst(): Node {
+    return this.head;
+  }
+
+  getLast(): Node {
+    return this.tail;
+  }
+
+  deleteList() {}
 }
